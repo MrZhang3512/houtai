a8a632eb540a12497cd8e4348d39fe4a0fe1f3ba
unknown <15235233600@sohu.com>
2020-10-10 23:05:54 +0800

    更改github地址

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -11,11 +11,8 @@
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
+          <a target="_blank" href="https://github.com/MrZhang3512/houtai.git">
             <el-dropdown-item>Github</el-dropdown-item>
-          </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
           </a>
           <el-dropdown-item divided @click.native="logout">
             <span style="display:block;">Log Out</span>
